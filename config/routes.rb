2a42cc4052fe31c671dd34e6f3863adc047226c2
Shogo Tobori <qb.brady.121212@gmail.com>
2020-07-27 13:54:45 +0000

    [Add]新規投稿画面、編集画面、詳細画面

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-	devise_for :users
-	root 'books#index'
-	resources :books, only: [:new, :create, :index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :books
+  root :to => 'books#top'
 end
